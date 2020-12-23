aa15ac9be75bccdb607724d54ad4cb69ca9bb915
pmgtrz <75604818+pamela-gutierrez@users.noreply.github.com>
2020-12-22 23:16:47 -0800

    add eventlisteners

--- script.js
+++ script.js
@@ -1,75 +1,121 @@
-var quizContainerElement  = document.getElementById("quiz-container");
-// either this or start button
-var startButton = document.getElementById("start-button")
-var questionElement =document.getElementById("question")
+// Setting the variables to be used in functions that will call on specific elements from the HTML
+var quizContainerElement = document.getElementById("quiz-container");
+var startButton = document.querySelector(".startBtn")
+var buttonA = document.getElementById('a')
+var buttonB = document.getElementById('b')
+var buttonC = document.getElementById('c')
+var buttonD = document.getElementById('d')
+var questionElement = document.getElementById("question")
+var buttons = document.getElementById("answer-buttons")
+var startingDiv = document.querySelector(".startingDiv")
 // var resultContainer = documet.getElementById("results");
-// var submitButton = document.getElementById("submit");
 
-// Event Listeners
-startButton.addEventListener('click', startGame)
+// Setting the variables to be used in functions but that are not called specifically from the HTML. They will instead be defined and re-defined by the functions executed
+var currentQuestion = 0
+var results;
+var totalSeconds;
+var timeElapsed;
+var timer
+var correctAnswerCount;
 
-var Quiz = [
+// Event Listeners that will "react" to different buttons being clicked and set off the next function
+startButton.addEventListener('click', startQuiz)
+buttonA.addEventListener('click', answerSelected)
+buttonB.addEventListener('click', answerSelected)
+buttonC.addEventListener('click', answerSelected)
+buttonD.addEventListener('click', answerSelected)
+
+// Functions that will actually execute the changes on page and keep track of data
+function startQuiz() {
+    startingDiv.style.display = "none"
+    quizContainerElement.style.display = "block"
+    showQuestion(quiz[currentQuestion])
+
+}
+
+//create timer
+
+//onclick for answer buttons
+//if this button's textContent === my correct answer, add one to current question and show next question
+//else (once timer is set up, subtract from timer)
+
+//display score once all questions have been answered
+
+function showQuestion(object) {
+    questionElement.innerText = object.question
+    buttonA.textContent = object.answers[0]
+    buttonB.textContent = object.answers[1]
+    buttonC.textContent = object.answers[2]
+    buttonD.textContent = object.answers[3]
+
+    // currentQuestion = currentQuestion + 1
+}
+
+function answerSelected() {
+    // if (buttonA.textContent === quiz[currentQuestion].correctAnswer) {
+    currentQuestion = currentQuestion + 1
+    showQuestion(quiz[currentQuestion])
+
+
+
+}
+
+
+function displayScore() {
+
+}
+var quiz = [
     {
         question: "Where is Central America located?",
-        answers: {
-            a: "North America",
-            b: "South America",
-            c: "It's an island in the Pacific",
-            d: "It's its own continent"
-        },
-        correctAnswer: "a"
+        answers: [
+            "North America",
+            "South America",
+            "It's an island in the Pacific",
+            "It's its own continent"
+        ],
+        correctAnswer: "North America"
     },
     {
-        questions: "How many countries are in Central America",
-        answers: {
-            a: "5",
-            b: "3",
-            c: "7",
-            d: "12",
-    },
-        correctAnswer: "c"
+        question: "How many countries are in Central America",
+        answers: [
+            "5",
+            "3",
+            "7",
+            "12",
+        ],
+        correctAnswer: "7"
     },
     {
         question: "Which country is not part of Central America?",
-        answers: {
-            a: "Panama",
-            b: "El Salvador",
-            c: "Belize",
-            d: "Mexico"
-        },
-        correctAnswer: "d"
+        answers: [
+            "Panama",
+            "El Salvador",
+            "Belize",
+            "Mexico"
+        ],
+        correctAnswer: "Mexico"
     },
     {
         question: "What is Central America known for?",
-            a: "Coffee",
-            b: "Volcanoes",
-            c: "Beaches",
-            d: "All of the above"
+        answers: [
+            "Coffee",
+            "Volcanoes",
+            "Beaches",
+            "All of the above"
+        ],
+        correctAnswer: "All of the above"
     },
     {
         question: "Should you visit Central America?",
-        answers: {
-            a: "No, I watch too much TV so I believe it's extremely dangous",
-            b: "No, I've already been to Mexico so I don't need to go there",
-            c: "No, I don't like to travel",
-            d: "YES!"
-        },
-        correctAnswer: "d"
+        answers: [
+            "No, I watch too much TV so I believe it's extremely dangous",
+            "No, I've already been to Mexico so I don't need to go there",
+            "No, I don't like to travel",
+            "YES!"
+        ],
+        correctAnswer: "YES!"
     }
 ];
 
-function startGame() {
-    console.log("started")
-    startButton.classList.add("hide")
-    quizContainerElement.classList.remove("hide")
-    setNextQuestion()
-}
 
-function nextQuestion() {
-
-}
-
-function selectAnswer() {
-
-}
 // If statement to say if they answer "X" then add one point to the total score, else take 10 seconds off timer
